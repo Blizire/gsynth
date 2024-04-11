7a88a100ca20846e3921606c65e0d0b40fd6aeb1
trenton stiles <stiles.trenton@gmail.com>
2024-04-10 19:05:21 -0500

    moved includes to main.h

--- include/main.h
+++ include/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <Windows.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <GLFW/glfw3.h>
-
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
 #endif
